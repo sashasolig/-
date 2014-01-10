473738253fd8eb88c7ee5bb99610607ce4379d7b
a.gurinovich <a.gurinovich89@gmail.com>
2014-01-10 16:45:58 +0300

    version 0.17

--- src/com/coolchoice/monumentphoto/dal/DatabaseHelper.java
+++ src/com/coolchoice/monumentphoto/dal/DatabaseHelper.java
@@ -36,8 +36,8 @@ public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
 	
     private final String LOG_TAG = getClass().getSimpleName();
     
-    public static final String DATABASE_NAME = "/mnt/sdcard/monument.db";
-    //public static final String DATABASE_NAME = "monument.db";
+    //public static final String DATABASE_NAME = "/mnt/sdcard/monument.db";
+    public static final String DATABASE_NAME = "monument.db";
     
     public static final int DATABASE_VERSION = 6;
     
